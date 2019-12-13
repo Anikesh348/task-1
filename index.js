14d2472efb7310cc379b854a686e4d3a8f698d9e
Anikesh348 <44468436+Anikesh348@users.noreply.github.com>
2019-12-13 20:59:18 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,57 +1,52 @@
 $(".paid").click(function() {
-  $(".active").css("visibility", "visible");
-  $(".nonactive").css("visibility", "collapse");
-  $('.active').attr('data-target','#exampleModalCenter1');
+  $(".active2").css("visibility", "visible");
+  $(".nonactive2").css("visibility", "collapse");
+  $('.active2').attr('data-target', '#exampleModalCenter1');
 });
-$(".free").click(function(){
-  $(".active").css("visibility", "visible");
-  $(".nonactive").css("visibility", "collapse");
-  $('.active').attr('data-target','#exampleModalCenter2');
+$(".free").click(function() {
+  $(".active2").css("visibility", "visible");
+  $(".nonactive2").css("visibility", "collapse");
+  $('.active2').attr('data-target', '#exampleModalCenter4');
 });
 $(".single").click(function() {
   $(".active1").css("visibility", "visible");
   $(".nonactive1").css("visibility", "collapse");
-  $('.active1').attr('data-target','#exampleModalCenter2');
+  $('.active1').attr('data-target', '#exampleModalCenter2');
 });
-$(".multiple").click(function(){
+$(".multiple").click(function() {
   $(".active1").css("visibility", "visible");
   $(".nonactive1").css("visibility", "collapse");
-  $('.active1').attr('data-target','#exampleModalCenter3');
+  $('.active1').attr('data-target', '#exampleModalCenter3');
 });
 var i = 0;
 var c = 0;
+var tprice = 0;
 $(".remove").click(function() {
   $(".cardp").css("visibility", "collapse");
-
 });
 $(".add").click(function() {
   $(".cardp").css("visibility", "visible");
   $(".done").html("Done 👍");
+  $(".done").css("visiblity", "visible");
+  $(".submit").css("visiblity", "collapse");
 });
 $(".done").click(function() {
-  var price = $(".test2").val();
   var category = $(".test1").val();
+  var price = $(".test2").val();
   if (price != " " && category != " ") {
     i++;
     c++;
   }
+  $(".tprices").text(" ");
+  tprice += Number(price);
   $(".test1").val(" ");
   $(".test2").val(" ");
   $(".test3").val(" ");
   if (c != 0) {
-    $(".price").append("<li>" + i + " 👉 " + category + " ₹" + price + "<br>" + "</li>");
+    $("ul").append("<li>" + i + " → " + category + " ₹" + price + "</li>");
     $("li").addClass("list-group-item");
     $(".cardp").css("visibility", "collapse");
     c = 0;
-    $(".done").html("Submit ✌");
-
+    $(".tprices").text("Total Cost:" + tprice);
   }
 });
-// $(".submit").click(function()
-// { var compare = $(".submit").html();
-//
-//   if(compare === "Submit ✌")
-//     {
-//       $('.done').attr("data-dismiss","modal");
-//     }
-// });
