2ebad0d6e5d3d15101e7b9802bb3eacc2f7ad1b8
Anikesh348 <44468436+Anikesh348@users.noreply.github.com>
2019-12-17 14:24:07 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -4,16 +4,19 @@ $(".paid").click(function() {
   $('.active2').attr('data-target', '#exampleModalCenter1');
 });
 $(".free").click(function() {
+
   $(".active2").css("visibility", "visible");
   $(".nonactive2").css("visibility", "collapse");
   $('.active2').attr('data-target', '#exampleModalCenter4');
 });
 $(".single").click(function() {
+
   $(".active1").css("visibility", "visible");
   $(".nonactive1").css("visibility", "collapse");
   $('.active1').attr('data-target', '#exampleModalCenter2');
 });
 $(".multiple").click(function() {
+
   $(".active1").css("visibility", "visible");
   $(".nonactive1").css("visibility", "collapse");
   $('.active1').attr('data-target', '#exampleModalCenter3');
@@ -24,13 +27,16 @@ var t = 0;
 var tprice = 0;
 $(".remove").click(function() {
   $(".cardp").css("visibility", "collapse");
+  $(".done").text("Submit ✌");
+  t = 1;
 });
 $(".add").click(function() {
   $(".cardp").css("visibility", "visible");
   $(".done").html("Done 👍");
   $(".done").removeAttr("data-dismiss", "modal");
-  t=0;
-  
+  t = 0;
+
+
 });
 $(".done").click(function() {
   t++;
@@ -41,23 +47,38 @@ $(".done").click(function() {
     c++;
 
   }
-  $(".tprices").text(" ");
-  tprice += Number(price);
   $(".test1").val(" ");
   $(".test2").val(" ");
   $(".test3").val(" ");
+  $(".tprices").text(" ");
+  tprice += Number(price);
+
   if (c != 0) {
-    $("ul").append("<li>" + i + " → " + category + " ₹" + price + "</li>");
-    $("li").addClass("list-group-item");
+    $(".entry").append("<li class=\"li\">" + i + " → " + category + " ₹" + price + "</li>");
+    $(".li").addClass("list-group-item");
     $(".cardp").css("visibility", "collapse");
     c = 0;
     $(".tprices").text("Total Cost:" + tprice);
     $(".done").text("Submit ✌");
+
   }
-  if (t == 2 ) {
+  else
+  {
+    if(t==1)
+    {swal("Error", "Please fill the form first!!", "error");
+      t=0;
+     }
+
+  }
+  if (t == 2) {
     $(".done").attr("data-dismiss", "modal");
+    swal("Congrats!", " Ticket Booked!", "success");
     t = 0;
+
   }
 
 
+});
+$(".done2").click(function(){
+  swal("Congrats!", " Ticket Booked!", "success");
 });
